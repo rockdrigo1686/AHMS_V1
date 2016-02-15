c546b7b5dcdaf30b1fcd911ef9187704756f8f77
jorge <jorge@jorge-HP-Pavilion-g4-Notebook-PC>
2016-02-14 19:30:23 -0600

    Merge origin/master
    Conflicts:
    	UI/AHMSSwingUI/src/com/ahms/ui/utils/UIConstants.java

---
+++ AHMS_Core/src/com/ahms/model/manager/entity_manager/MultiValueEM.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ahms.model.manager.entity_manager;
+
+import com.ahms.model.entity.MultiValue;
+import com.ahms.model.manager.AHMSEntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author jorge
+ */
+public class MultiValueEM extends AHMSEntityManager {
+    
+    public MultiValue findByKey(MultiValue multivalue) {
+        try {
+            if (em == null || !em.isOpen()) {
+                createEm();
+            }
+            TypedQuery<MultiValue> query = em.createNamedQuery("Account.findByMvaKey", MultiValue.class);
+            query.setParameter("mvaKey", multivalue.getMvaKey());
+            return query.getSingleResult();
+        } catch (Exception e) {
+            if (e instanceof NoResultException) {
+                return null;
+            } else {
+                throw e;
+            }
+        } finally { 
+            if (em != null) {
+                closeEm();
+            }
+        }
+    }
+    
+}
