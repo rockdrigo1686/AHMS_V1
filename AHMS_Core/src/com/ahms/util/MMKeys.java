8223c6a62a651e1424c508bfb83f7f9a1aec3667
rsoto <rsoto@rsoto-Dell-System-XPS-L502X>
2016-05-08 13:40:19 -0500

    Merge origin/master
    Conflicts:
    	AHMS_Core/src/com/ahms/model/entity/Users.java

--- AHMS_Core/src/com/ahms/util/MMKeys.java
+++ AHMS_Core/src/com/ahms/util/MMKeys.java
@@ -10,8 +10,9 @@ package com.ahms.util;
  * @author rsoto
  */
 public class MMKeys {
-     public static class Rooms{
-        
+
+    public static class Rooms {
+
         public final static String GP_KEY = "RMS";
         public final static String STA_DISPONIBLE = "Disponible";
         public final static String STA_DISPONIBLE_KEY = "AV";
@@ -22,9 +23,9 @@ public class MMKeys {
         public final static String STA_MTO = "Mantenimiento";
         public final static String STA_MTO_KEY = "MT";
     }
-    
-   public static class Acounts{
-        
+
+    public static class Acounts {
+
         public final static String GP_KEY = "ACT";
         public final static String STA_ABIERTO = "Abierto";
         public final static String STA_ABIERTO_KEY = "ACT_A";
@@ -33,19 +34,20 @@ public class MMKeys {
         public final static String STA_MOROSO = "Moroso";
         public final static String STA_MOROSO_KEY = "ACT_P";
     }
-   
-   public static class AccountsTransactions {
-       public final static String GP_KEY = "ACTR";
-       public final static String STA_PAGADO = "Pagado";
-       public final static String STA_PAGADO_KEY = "PAID";
-       public final static String STA_PENDIENTE = "Pendiente";
-       public final static String STA_PENDIENTE_KEY = "PEND";
-       public final static String STA_CANCELADO = "Cancelado";
-       public final static String STA_CANCELADO_KEY = "CANC";
-   }
 
-    public static class Shift{
-        
+    public static class AccountsTransactions {
+
+        public final static String GP_KEY = "ACTR";
+        public final static String STA_PAGADO = "Pagado";
+        public final static String STA_PAGADO_KEY = "PAID";
+        public final static String STA_PENDIENTE = "Pendiente";
+        public final static String STA_PENDIENTE_KEY = "PEND";
+        public final static String STA_CANCELADO = "Cancelado";
+        public final static String STA_CANCELADO_KEY = "CANC";
+    }
+
+    public static class Shift {
+
         public final static String GP_KEY = "SHF";
         public final static String STA_ABIERTO = "Abierto";
         public final static String STA_ABIERTO_KEY = "SHF_O";
@@ -53,8 +55,8 @@ public class MMKeys {
         public final static String STA_CERRADO_KEY = "SHF_E";
     }
 
-   public static class General{
-        
+    public static class General {
+
         public final static String GP_KEY = "GRL";
         public final static String STA_ACTIVO = "Activo";
         public final static String STA_ACTIVO_KEY = "AC";
@@ -62,10 +64,17 @@ public class MMKeys {
         public final static String STA_INACTIVO_KEY = "X";
     }
 
-    
-    public static class Math{
-        
+    public static class Math {
+
         public final static String GP_KEY = "MTH";
         public final static String IVA_KEY = "IVA";
+    }
+
+    public static class Profiles {
+        public final static String ADMI = "ADMI";
+        public final static String CASH = "CASH";
+        public final static String ACNT = "ACNT";
+        public final static String MNGR = "MNGR";
+        public final static String EMP = "EMP";
     }
 }
