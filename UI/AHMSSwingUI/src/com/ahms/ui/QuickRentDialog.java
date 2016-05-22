ae5638eeb6c55de96bfe31f7ba18da9e633cc16e
jorge <jorge@jorge-HP-Pavilion-g4-Notebook-PC>
2016-05-21 21:28:55 -0500

    Merge origin/master
    Conflicts:
    	UI/AHMSSwingUI/src/com/ahms/ui/utils/UIConstants.java

--- UI/AHMSSwingUI/src/com/ahms/ui/QuickRentDialog.java
+++ UI/AHMSSwingUI/src/com/ahms/ui/QuickRentDialog.java
@@ -546,6 +546,9 @@ public class QuickRentDialog extends javax.swing.JDialog {
                     jlNumber.setText(jlNumber.getText() + "  " +  roomAvailableByType.getRmsNumber());
                 }                
                 jbtnLoadCustomer.setEnabled(true);
+                if(roomAvailableByTypeLst.size() < numRooms){
+                   GeneralFunctions.sendMessage(this, "De los " + numRooms + " que se especificaron, solo " + roomAvailableByTypeLst.size() + " estan disponibles.");
+                }
             } else {
                 GeneralFunctions.sendMessage(this, UIConstants.NO_AVAIL_ROOMS);
                 jbtnLoadCustomer.setEnabled(false);
